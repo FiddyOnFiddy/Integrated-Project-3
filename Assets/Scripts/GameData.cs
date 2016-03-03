f6d68f3787724f40a2545e06a18f7cf1095fc31f
MattBee2k2 <matthewbiggins@hotmail.co.uk>
2016-03-03 22:35:16 +0000

    Changed/Added things
    Building interaction reworked and simple stat system implemented
    recording cost of buildings.

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameData : MonoBehaviour
 {
+    public int pollutionLevel = 100;
+    public int money = 200;
+
+    public Text moneyText;
+    public Text pollutionText;
 
 	// Use this for initialization
 	void Start ()
@@ -13,6 +19,8 @@ public class GameData : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-	
+        moneyText.text = "Money: $" + money.ToString();
+        pollutionText.text = "Pollution: " + pollutionLevel.ToString() + "%";
 	}
+
 }
