80e02c9890e7f5babaa34efa10d993fd5e82b892
MattBee2k2 <matthewbiggins@hotmail.co.uk>
2016-04-12 21:36:22 +0100

    fixed controls text

---
+++ Assets/Scripts/HideControlsText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HideControlsText : MonoBehaviour
+{
+
+    public Text controls1, controls2;
+
+	// Use this for initialization
+	void Start () 
+    {
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == 1)
+        {
+            controls1.enabled = false;
+            controls2.enabled = false;
+        }
+    }
+}
